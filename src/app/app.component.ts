8f1975aa4a96e9ab0f2d7d26ad1f79f2e7d7de7c
Tsuyoshi HARA <chloe463+git@gmail.com>
2018-10-25 13:55:22 +0900

    refactor: Use async pipe in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from './entities/todo';
 
+import { Observable } from 'rxjs';
 import { switchMapTo } from 'rxjs/operators';
 
 import { ApiService } from './services/api.service';
@@ -13,21 +14,19 @@ import { ApiService } from './services/api.service';
 export class AppComponent implements OnInit {
   text: string;
 
-  todos: Todo[];
+  todos$: Observable<Todo[]>;
 
   constructor(private api: ApiService) {
   }
 
   ngOnInit() {
-    this.api.getTodos().subscribe(v => this.todos = v);
+    this.todos$ = this.api.getTodos();
   }
 
   addTodo(event) {
     const todo = new Todo(event);
-    this.api.addTodo(todo).pipe(
+    this.todos$ = this.api.addTodo(todo).pipe(
       switchMapTo(this.api.getTodos())
-    ).subscribe(
-      v => this.todos = v
     );
   }
 }
